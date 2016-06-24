0dd02afe5d653f92efad7124e902a71ba8ef627f
sdavidk <davidkohno500@gmail.com>
2016-06-24 12:06:57 -0400

    Initial commit

---
+++ Virtual-Pet-David/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Virtual_Pet_David
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
